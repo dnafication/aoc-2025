cd82daf46f1863b27295d7bd5d306430fd9a3b6d
Dina <dnafication@users.noreply.github.com>
2025-12-11 17:36:08 +1100

    feat: implement initial solution for day11 with pathfinding logic and input data

---
+++ day11/main.py
@@ -0,0 +1,46 @@
+import os
+
+
+def solve_part1(lines):
+    print("Solving Part 1")
+    network = {}
+    for line in lines:
+        name, output = line.strip().split(":")
+        output = output.strip().split(" ")
+        # print(f"Device Name: {name}, Output: {output}")
+        network[name] = output
+    # print(network)
+
+    def find_all_paths(network, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return [path]
+        if start not in network:
+            return []
+        paths = []
+        for node in network[start]:
+            if node not in path:
+                newpaths = find_all_paths(network, node, end, path)
+                for newpath in newpaths:
+                    paths.append(newpath)
+        return paths
+
+    all_paths = find_all_paths(network, "you", "out")
+    print(f"Count of all paths from 'you' to 'out': {len(all_paths)}")
+
+
+def solve_part2(lines):
+    print("Solving Part 2")
+
+
+def main():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    # Replace with test.txt for testing with smaller input
+    with open(os.path.join(script_dir, "input.txt"), "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        solve_part1(lines)
+        solve_part2(lines)
+
+
+if __name__ == "__main__":
+    main()
